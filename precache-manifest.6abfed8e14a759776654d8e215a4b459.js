1c6434250d97d7a77de7be2bf447d78d9c32569d
philipp4tabs <41897644+philipp4tabs@users.noreply.github.com>
2018-07-30 17:33:02 +0400

    Add files via upload

---
+++ precache-manifest.6abfed8e14a759776654d8e215a4b459.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d5c144daf778eb0777e6",
+    "url": "/js/chunk-vendors.d5c144da.js"
+  },
+  {
+    "revision": "f3465c06b4cb7ff51e4a",
+    "url": "/js/app.f3465c06.js"
+  },
+  {
+    "revision": "33c65f9807874eb4a18e9eeee3616ac8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5c144daf778eb0777e6",
+    "url": "/css/chunk-vendors.9fe88aa2.css"
+  },
+  {
+    "revision": "f3465c06b4cb7ff51e4a",
+    "url": "/css/app.c09c0c04.css"
+  }
+];
